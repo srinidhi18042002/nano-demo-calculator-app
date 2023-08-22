989ddd6cb992269bb0aecd8e0898662c94c51f57
srinidhi18042002 <srinidhi.pes143@gmail.com>
2023-08-22 22:39:15 +0530

    update

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,17 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"};
 }
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    auto res = input['first'] + input['second'];
+    return crow::response{"result":res};
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
+    auto res = input['first'] - input['second'];
     return crow::response{""};
 }
